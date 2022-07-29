a1dedede297aea1a82c24e7090fdbaf1ea54c1fa
Glaudiston Gomes da Silva <c_ggomes@ojolabs.com>
2022-07-29 17:10:07 -0300

    merge fix

--- home-academy-mfe/src/Routes.tsx
+++ home-academy-mfe/src/Routes.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes as ReactRoutes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes as ReactRoutes, Route } from "react-router-dom";
 import Home from "./Home";
 import HowToBuy from "./HowToBuy";
 import Purpose from "./Purpose";
+import NewPage from "./NewPage";
+import StartPage from "./StartPage";
 
 const Routes: React.FC<{}> = _ => {
 	return (
@@ -11,6 +13,8 @@ const Routes: React.FC<{}> = _ => {
 			<Route path="/" element={<Home />} />
 			<Route path="/howtobuy" element={<HowToBuy />} />
 			<Route path="/purpose" element={<Purpose />} />
+			<Route path="/newpage" element={<NewPage />} />
+			<Route path="/start" element={<StartPage />} />
 		</ReactRoutes>
 	</Router>
 	)
