955bb0106046bad9a4807d556a56624d8effb3fd
Vir Sharad <sardesai.aarah@gmail.com>
2022-07-29 14:21:27 -0500

    edited and added some features on the components

--- home-academy-mfe/src/Purpose.tsx
+++ home-academy-mfe/src/Purpose.tsx
@@ -5,7 +5,14 @@ const Purpose: React.FC<{}> = props => {
   const [click, setClick] = useState<boolean>(false);
 	return (
     <div className="App">
+		
 	{Head("Our Purpose", click, setClick )}
+	<div>
+	<img src="./images/purpose.png" className={click ? 'show-menu App-logo' : 'hide-menu App-logo'}  alt="logo" />
+	</div>
+	Often families in old, impoverished towns with primary infrastructure and little to none recreational events raise children who go to the nearby impoverished schools with basic funding and a lack of opportunities to branch. This cycle continues and raises generations that are less likely to expand and attempt to buy a home due to lack of education. To help break this cycle, we wanted to build a website that offers free and trustworthy content to provide to people that have little to none knowledge in buying a house and help guide them through the intricate process of home-buying confidently.   
+
+
 		</div>
 	)
 }
